ae98d8988eac31ca84bc993dc5dd8c00607bbfd8
Caio Gabriel <130727060+caiogbiel@users.noreply.github.com>
2024-04-11 12:30:05 -0300

    Update Hospital.java

--- Atividade_1/src/main/java/com/mycompany/hospital/Hospital.java
+++ Atividade_1/src/main/java/com/mycompany/hospital/Hospital.java
@@ -2,6 +2,18 @@ package com.mycompany.hospital;
 public class Hospital {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // Criando médicos
+        Medico medico1 = new Medico("Dr. João", "12345678900", "Clínico Geral", "CRM123");
+        Medico medico2 = new Medico("Dra. Maria", "98765432100", "Pediatra", "CRM456");
+        // Criando pacientes
+        Paciente paciente1 = new Paciente("Fulano", "11122233344", true);
+        Paciente paciente2 = new Paciente("Ciclano", "22233344455", false);
+        // Criando gerenciador
+        Gerenciador gerenciador = new Gerenciador();
+        // Agendando consultas
+        gerenciador.cadastrarConsulta(medico1, paciente1, 10);
+        gerenciador.cadastrarConsulta(medico2, paciente2, 15);
+        // Listando consultas
+        gerenciador.listarConsultasTodas();
     }
 }
