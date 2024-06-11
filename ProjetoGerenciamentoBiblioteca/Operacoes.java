a37b84f4d0d2329d7f0bff6ea52847a5e96ae6a3
Caio Gabriel <130727060+caiogbiel@users.noreply.github.com>
2024-06-11 15:22:14 -0300

    atualizacao operacoes biblioteca

--- ProjetoGerenciamentoBiblioteca/Operacoes.java
+++ ProjetoGerenciamentoBiblioteca/Operacoes.java
@@ -1,7 +1,7 @@
 public interface Operacoes {
     void adicionarLivro(Livro livro);
-    void emprestarLivro(Usuario usuario, Livro livro);
-    void devolverLivro(Livro livro);
+    void emprestarLivro(Usuario usuario, Livro livro)throws Exception;
+    void devolverLivro(Usuario usuario, Livro livro)throws Exception;
     void excluirLivro(Livro livro);
     void pesquisarLivro(String criterio);
     void verificarSituacaoUsuario(Usuario usuario);    
--- ProjetoGerenciamentoBiblioteca/OperacoesBiblioteca.java
+++ ProjetoGerenciamentoBiblioteca/OperacoesBiblioteca.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
 
 public class OperacoesBiblioteca implements Operacoes{
 
@@ -23,18 +25,83 @@ public class OperacoesBiblioteca implements Operacoes{
     }
 
     @Override
-    public void emprestarLivro(Usuario usuario, Livro livro) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void emprestarLivro(Usuario usuario, Livro livro) throws Exception{
+       // verificar se existe livro, usuario e a situacao do livro
+        if (!banco.listarUsuarios().contains(usuario)) {
+            throw new Exception("Usuário não encontrado.");
+        }
+        if (!banco.listarLivros().contains(livro)) {
+            throw new Exception("Livro não encontrado.");
+        }
+        if (livro.getStatus() == StatusLivro.EMPRESTADO) {
+            throw new Exception("Livro já está emprestado.");
+        }
+        
+        long emprestimosUsuario = banco.listarEmprestimosAtivos().stream().filter(emprestimo-> emprestimo.getUsuario().equals(usuario)).count();
+        
+        //verificar se usuario atingiu limite de emprestimo
+        if(usuario instanceof Estudante && emprestimosUsuario >= 3) {
+            throw new Exception("Estudantes podem pegar no máximo 3 livros.");
+        }
+        else if ((usuario instanceof Professores || usuario instanceof Bibliotecario) && emprestimosUsuario >= 5) {
+            throw new Exception("Professores e Bibliotecários podem pegar no máximo 5 livros.");
+        }
+        
+        livro.setStatus(StatusLivro.EMPRESTADO); //atualiza status
+        
+        //configura data devolucao pelo usuario
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DAY_OF_MONTH, usuario instanceof Estudante ? 15 : 30);
+        Date dataDevolucao = cal.getTime();
+        
+        //fazer emprestimo
+        Emprestimo emprestimo = new Emprestimo(usuario, livro, new Date(), dataDevolucao);
+        banco.adicionarEmprestimo(emprestimo);
     }
 
     @Override
-    public void devolverLivro(Livro livro) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void devolverLivro(Usuario usuario, Livro livro) {
+        try{
+            if(banco.listarUsuarios().contains(usuario)){
+                throw new Exception("Usuario nao encontrado");
+            }
+            if(banco.listarLivros().contains(livro)){
+                throw new Exception("Livro nao encontrado");
+            }
+            
+            Emprestimo emprestimoRemove = null;
+            for(Emprestimo emprestimo : banco.listarEmprestimosAtivos()) {
+                if(emprestimo.getUsuario().equals(usuario) && emprestimo.getLivro().equals(livro)){
+                    emprestimoRemove = emprestimo;
+                    break;
+                }
+            }
+            
+            if(emprestimoRemove == null){
+                throw new Exception("Emprestimo nao encontrado");
+            }
+            
+            //muda status livro
+            livro.setStatus(StatusLivro.DISPONIVEL);
+            //remove emprestimo
+            banco.removerEmprestimo(emprestimoRemove);
+        } catch(Exception e) {
+            System.err.println("Erro ao devolver livro: " + e.getMessage());
+        } 
+            
     }
+    
 
     @Override
     public void excluirLivro(Livro livro) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(!banco.listarLivros().contains(livro)){
+            System.out.println("Livro nao encontrado no banco DAO");
+        }
+        if(livro.getStatus() == StatusLivro.EMPRESTADO) {
+            System.out.println("Nao é possivel excluir um livro emprestado");
+        }
+        banco.removerLivro(livro);
     }
 
     @Override
