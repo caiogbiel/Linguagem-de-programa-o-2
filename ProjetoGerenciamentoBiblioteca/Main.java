327f5b5f90118dbb979c3077f08f090bce0e0f04
Caio Gabriel <caiogbiel13@gmail.com>
2024-06-19 16:44:24 -0300

    começo da execução main

--- ProjetoGerenciamentoBiblioteca/Main.java
+++ ProjetoGerenciamentoBiblioteca/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-       
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+    }  
 }
