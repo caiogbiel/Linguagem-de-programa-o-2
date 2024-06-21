c9825a1e978bf403c2ab310e910ebb53a243e7f7
Caio Gabriel <caiogbiel13@gmail.com>
2024-06-21 11:11:44 -0300

    retorno projeto antigo

--- ProjetoGerenciamentoBiblioteca/Main.java
+++ ProjetoGerenciamentoBiblioteca/Main.java
@@ -1,20 +1,6 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
-public class Main extends Application{
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
-        primaryStage.setTitle("Sistema de Biblioteca");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }  
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        
     }
 }
