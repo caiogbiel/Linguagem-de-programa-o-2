813d7fbe1ba89bc8c36758047e618a3433bb1aa5
Caio Gabriel <caiogbiel13@gmail.com>
2024-06-24 16:47:58 -0300

    ajeitando alguns design

--- GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/App.java
+++ GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/App.java
@@ -1,20 +1,19 @@
 package com.example.biblioteca;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.layout.VBox;
-
-import java.io.IOException;
 
 public class App extends Application {
     private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("tLogin.fxml"));
-        scene = new Scene(root, 640, 480);
+        scene = new Scene(root, 700, 500);
         stage.setScene(scene);
         stage.show();
     }
