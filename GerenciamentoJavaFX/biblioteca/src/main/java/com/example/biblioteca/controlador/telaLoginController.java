dbdca6ba0e2fa67664e114a922da4cdd885f3145
Caio Gabriel <caiogbiel13@gmail.com>
2024-06-24 12:44:46 -0300

    alerta erro senha

--- GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/controlador/telaLoginController.java
+++ GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/controlador/telaLoginController.java
@@ -1,13 +1,15 @@
 package com.example.biblioteca.controlador;
 
+import java.io.IOException;
+
 import com.example.biblioteca.App;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class telaLoginController {
 
@@ -27,6 +29,14 @@ public class telaLoginController {
     private void trocarTela() throws IOException {
         if(tUser.getText().equalsIgnoreCase("admin") && tSenha.getText().equalsIgnoreCase("admin123")) {
             App.trocarLayout("tFunctions.fxml");
+        }
+        else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro de Login");
+            alert.setHeaderText("Usuário ou senha incorretos");
+            alert.setContentText("Por favor, verifique seu usuário e senha e tente novamente.");
+
+            alert.showAndWait();
         }
     }
 }
