e6bd8f72d12dc018538b70e16f31b4c745c9b9cc
Caio Gabriel <caiogbiel13@gmail.com>
2024-06-25 14:51:08 -0300

    tentando melhorar design

--- GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/controlador/telaAddController.java
+++ GerenciamentoJavaFX/biblioteca/src/main/java/com/example/biblioteca/controlador/telaAddController.java
@@ -1,14 +1,36 @@
 package com.example.biblioteca.controlador;
 
+import java.io.IOException;
+
 import com.example.biblioteca.App;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 public class telaAddController {
     
+    @FXML
+    private TextField addAssunto;
+
+    @FXML
+    private TextField addAutor;
+
+    @FXML
+    private DatePicker addDataLancamento;
+
+    @FXML
+    private TextField addEstoque;
+
+    @FXML
+    private TextField addTitutlo;
+
+    @FXML
+    private Button confirmarButton;
+
+    @FXML
+    private void voltar() throws IOException {
+        App.trocarLayout("tFunctions.fxml"); // Voltar para a tela de funções
+    }
 }
